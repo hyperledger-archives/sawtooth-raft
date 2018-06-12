4d9575ed67d4d7ac485e6c480690ee39bec04880
Adam Ludvik <ludvik@bitwise.io>
2018-06-12 13:54:30 -0500

    Use RaftConfig defaults specified in docs
    The raft-rs documentation provides a better example of how to set the
    RaftConfig defaults.
    
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- src/config.rs
+++ src/config.rs
@@ -23,32 +23,13 @@ pub const RAFT_PERIOD: Duration = Duration::from_millis(100);
 pub const PUBLISH_PERIOD: Duration = Duration::from_secs(3);
 
 pub fn raft_config() -> Config {
-    Config {
-        // The unique ID for the Raft node.
-        id: 1,
-        // The Raft node list.
-        // Mostly, the peers need to be saved in the storage
-        // and we can get them from the Storage::initial_state function, so here
-        // you need to set it empty.
-        peers: vec![1],
-        // Election tick is for how long the follower may campaign again after
-        // it doesn't receive any message from the leader.
-        election_tick: 10,
-        // Heartbeat tick is for how long the leader needs to send
-        // a heartbeat to keep alive.
-        heartbeat_tick: 3,
-        // The max size limits the max size of each appended message. Mostly, 1 MB is enough.
-        max_size_per_msg: 1024 * 1024 * 1024,
-        // Max inflight msgs that the leader sends messages to follower without
-        // receiving ACKs.
-        max_inflight_msgs: 256,
-        // The Raft applied index.
-        // You need to save your applied index when you apply the committed Raft logs.
-        applied: 0,
-        // Just for log
-        tag: format!("[{}]", 1),
-        ..Default::default()
-    }
+    let mut config = Config::default();
+    config.id = 1;
+    config.heartbeat_tick = 150;
+    config.election_tick = config.heartbeat_tick * 10;
+    config.max_inflight_msgs = 10;
+    config.validate().expect("Invalid Raft Config");
+    config
 }
 
 pub fn storage() -> MemStorage {
